c0aad7611f8e0c514f20530d92824a6a6f78bb1b
cedar445 <lsk156278@outlook.com>
2024-03-13 18:27:46 +0800

    assigment3

---
+++ assigment3/problem1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rectangle rec = new Rectangle(2, 3);
+                Console.WriteLine($"Area of rectangle is {rec.Area()}");
+                Square sq = new Square(3, 4);
+                Console.WriteLine($"Area of Square is {sq.Area()}");
+                Triangle tr = new Triangle(3, 4);
+                Console.WriteLine($"Area of Triangle is {tr.Area()}");
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("something went wrong");
+            }
+        }
+    }
+    public class Rectangle
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            if(width<=0|| height<=0)
+            {
+                throw new ArgumentException("width and heigth must bigger than 0");
+            }
+        }
+        public int Area()
+        {
+            return width * height;
+        }
+    }
+    public class Square : Rectangle
+    {
+        public Square(int width, int height):base(width, height) { }
+    }
+    public class Triangle
+    {
+        public int bottom {  get; set; }
+        public int height { get; set; }
+        public Triangle(int bottom, int height)
+        {
+            this.bottom = bottom;
+            this.height = height;
+            if (bottom<=0 || height <= 0)
+            {
+                throw new ArgumentException("bootom and height must bigger than 0");
+            }
+        }
+        public double Area ()
+        {
+            double tmp = bottom * height;
+            return tmp / 2;
+        }
+    }
+}
---
+++ assigment3/problem2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleFactory factory = new SimpleFactory();
+            List<MyShape> shapes = factory.Create();
+            Console.WriteLine(factory.AreaSum(shapes));
+            Console.ReadKey();
+        }
+    }
+    public abstract class Shape
+    {
+        public abstract int Area();
+    }
+    public class MyShape:Shape
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public MyShape(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        override public int Area () { return width * height; }
+    }
+    public class SimpleFactory
+    {
+        public SimpleFactory() { }
+        public List<MyShape> Create()
+        {
+            List<MyShape> ms=new List<MyShape>();
+            for(int i = 0; i < 10; i++)
+            {
+                ms.Add(new MyShape(i, i));
+            }
+            return ms;
+        }
+        public int AreaSum(List<MyShape> ms)
+        {
+            int sum = 0;
+            foreach(MyShape m in ms)
+            {
+                sum += m.Area();
+            }
+            return sum;
+        }
+    }
+}
