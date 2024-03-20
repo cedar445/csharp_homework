cbea02226c3bf26f178a1300106ef8963d754eeb
cedar445 <lsk156278@outlook.com>
2024-03-20 19:28:50 +0800

    assignment4 and name changed

---
+++ assignment4/problem1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace problem1
+{
+    public class Node<T>
+    {
+        public Node<T> next;
+        public T value;
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        private T sum;
+        private T max;
+        private T min;
+        public GenericList()
+        {
+            head = tail = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+                sum = t;
+                max = t;
+                min = t;
+            }
+            else
+            {
+                tail.next = n;
+                tail = n;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist= new GenericList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                intlist.Add(i);
+            }
+            int sum = 0;
+            int max = intlist.Head.value;
+            int min = intlist.Head.value;
+            Action<int> getSum = m => sum += m;
+            Action<int> getMax = m => max = (max > m) ? max : m;
+            Action<int> getMin = m => min = (min < m) ? min : m;
+            Node<int> n=intlist.Head;
+            while(n != null )
+            {
+                getSum(n.value);
+                getMax(n.value);
+                getMin(n.value);
+                n=n.next;
+            }
+            Console.WriteLine($"SUM:{sum}");
+            Console.WriteLine($"MAX:{max}");
+            Console.WriteLine($"MIN:{min}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ assignment4/problem2/Program.cs
@@ -0,0 +1,71 @@
+﻿using problem2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+    public class AlarmEventArgs
+    {
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+    }
+    public delegate void AlarmDelegate(Object sender, AlarmEventArgs ea);
+    public class Alarm
+    {
+        public event AlarmDelegate OnTick;
+        public event AlarmDelegate OnAlarm;
+        public void tick(int h, int m, int s)
+        {
+            Console.WriteLine($"Alarm is ticking on {h}:{m}:{s}");
+            AlarmEventArgs ea = new AlarmEventArgs()
+            {
+                hour = h,
+                minute = m,
+                second = s
+            };
+            OnTick(this, ea);
+        }
+        public void alarm(int h, int m, int s)
+        {
+            Console.WriteLine($"Alarm is alarming on {h}:{m}:{s}");
+            AlarmEventArgs ea = new AlarmEventArgs()
+            {
+                hour = h,
+                minute = m,
+                second = s
+            };
+            OnAlarm(this, ea);
+        }
+    }
+    public class Test
+    {
+        public Alarm al = new Alarm();
+        public Test()
+        {
+            al.OnTick += new AlarmDelegate(Alarm_Tick);
+            al.OnAlarm += new AlarmDelegate(Alarm_Alarm);
+        }
+        void Alarm_Tick(object sender, AlarmEventArgs aea)
+        {
+            Console.WriteLine("alarm is ticking!");
+        }
+        void Alarm_Alarm(object sender, AlarmEventArgs aea)
+        {
+            Console.WriteLine("alarm is alarming!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.al.tick(0, 0, 0);
+            test.al.alarm(12, 12, 12);
+            Console.ReadKey();
+        }
+    }
+}
